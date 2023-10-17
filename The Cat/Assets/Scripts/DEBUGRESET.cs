2bd1f1751c907fda379d4aa07ac0db4c6f2f2b2c
Dogmaruz <i@john78.ru>
2023-10-17 15:07:23 +0300

    Сделал Level_04

--- The Cat/Assets/Scripts/DEBUGRESET.cs
+++ The Cat/Assets/Scripts/DEBUGRESET.cs
@@ -5,14 +5,26 @@ public class DEBUGRESET : MonoBehaviour
 {
     private CoinManager _coinManager;
 
+    private MovementController _movementController;
+
     [Inject]
-    public void Construct(CoinManager coinManager)
+    public void Construct(CoinManager coinManager, LevelSecuenceController levelSecuenceController)
     {
         _coinManager = coinManager;
     }
 
+    private void Awake()
+    {
+        _movementController = GetComponent<MovementController>();
+    }
+
     private void Update()
     {
-        if(Input.GetKeyUp(KeyCode.R)) { _coinManager.ResetValue(); }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            _coinManager.ResetValue();
+
+            _movementController._isLose = true;
+        }
     }
 }
